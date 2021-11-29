ed63c58054436fccaa3955f50ac44a6d855e8c73
Дюбанова Мария Борисовна <dyubanova.mb@esphere.ru>
2021-11-30 02:28:49 +0300

    module2-task2

---
+++ source/js/animation/constants.js
@@ -0,0 +1,25 @@
+export const ACCENT_TYPOGRAPHY_ORDERS = {
+  INTRO: [[3, 1, 0, 1, 2, 1, 0, 4, 2, 0, 2, 0], [2, 3, 1, 0, 2, 0]],
+  STORY: [3, 1, 0, 1, 2, 1, 0],
+  PRIZES: [2, 1, 0, 1, 2],
+  RULES: [4, 2, 1, 0, 3, 2, 0],
+  GAME: [3, 1, 0, 1],
+  DATE: [2, 1, 2, 3, 0, 1, 4, 1, 0, 3, 2, 1]
+};
+
+export const TITLE_CLASSES = {
+  INTRO: `.intro__title`,
+  STORY: `.slider__item-title`,
+  PRIZES: `.prizes__title`,
+  RULES: `.rules__title`,
+  GAME: `.game__title`,
+  DATE: `.intro__date`,
+};
+
+export const LOCATIONS = {
+  INTRO: `top`,
+  STORY: `story`,
+  PRIZES: `prizes`,
+  RULES: `rules`,
+  GAME: `game`,
+};
